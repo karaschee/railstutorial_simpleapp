6f174c6cd1cca6d48c6ac466e2cc9238b243b91e
karaschee <baishihaojc@gmail.com>
2013-12-19 16:59:15 +0800

    complete the book

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,20 @@ class Micropost < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+
+  # def self.from_users_followed_by(user)
+  #   # followed_user_ids = user.followed_user.map(&:id)
+  #   followed_user_ids = user.followed_user_ids
+  #   # where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+  #   where("user_id IN (:followed_user_ids) OR user_id = :user_id",
+  #     followed_user_ids: followed_user_ids, user_id: user)
+  # end
+
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+          user_id: user.id)
+  end
+
 end
